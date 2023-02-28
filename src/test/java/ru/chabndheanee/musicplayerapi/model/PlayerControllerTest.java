c3c6af7e0b46b1042bfa85762ddb44adcbf6e95f
Daniil <rusbear060@gmail.com>
2023-02-28 14:07:01 +0300

    change player controller to work with song name and change tests

--- src/test/java/ru/chabndheanee/musicplayerapi/model/PlayerControllerTest.java
+++ src/test/java/ru/chabndheanee/musicplayerapi/model/PlayerControllerTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 public class PlayerControllerTest {
     @Test
     void testPlaying_AndPausing_AndExpectNoError() {
-        PlayerController.addSong("C:\\Users\\rusbe\\dev\\music-player-api\\src\\main\\java\\ru\\chabndheanee\\musicplayerapi\\src\\532_full_dynamic-upbeat-logo_0012_preview.wav");
-        PlayerController.addSong("C:\\Users\\rusbe\\dev\\music-player-api\\src\\main\\java\\ru\\chabndheanee\\musicplayerapi\\src\\Metallica_-_Nothing_Else_Matters_(ColdMP3.com).wav");
+        PlayerController.addSong("532_full_dynamic-upbeat-logo_0012_preview");
+        PlayerController.addSong("Metallica_-_Nothing_Else_Matters_(ColdMP3.com)");
         PlayerController.play();
         PlayerController.pause();
         PlayerController.play();
         PlayerController.next();
         PlayerController.prev();
-        PlayerController.deleteSong("C:\\Users\\rusbe\\dev\\music-player-api\\src\\main\\java\\ru\\chabndheanee\\musicplayerapi\\src\\Metallica_-_Nothing_Else_Matters_(ColdMP3.com).wav");
+        PlayerController.deleteSong("Metallica_-_Nothing_Else_Matters_(ColdMP3.com)");
     }
 }
