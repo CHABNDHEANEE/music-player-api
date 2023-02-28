fb63c34b48d732f2f6dc1dfd8c79a52d4f0eacf7
Daniil <rusbear060@gmail.com>
2023-02-28 13:51:09 +0300

    add tests for PlayerController

--- src/test/java/ru/chabndheanee/musicplayerapi/model/PlayerControllerTest.java
+++ src/test/java/ru/chabndheanee/musicplayerapi/model/PlayerControllerTest.java
@@ -2,16 +2,16 @@ package ru.chabndheanee.musicplayerapi.model;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-
 public class PlayerControllerTest {
     @Test
     void testPlaying_AndPausing_AndExpectNoError() {
-        String input = "C:\\Users\\rusbe\\dev\\music-player-api\\src\\main\\java\\ru\\chabndheanee\\musicplayerapi\\src\\532_full_dynamic-upbeat-logo_0012_preview.wav";
-        ByteArrayInputStream bais = new ByteArrayInputStream(input.getBytes());
-        System.setIn(bais);
-        PlayerController.addSong();
+        PlayerController.addSong("C:\\Users\\rusbe\\dev\\music-player-api\\src\\main\\java\\ru\\chabndheanee\\musicplayerapi\\src\\532_full_dynamic-upbeat-logo_0012_preview.wav");
+        PlayerController.addSong("C:\\Users\\rusbe\\dev\\music-player-api\\src\\main\\java\\ru\\chabndheanee\\musicplayerapi\\src\\Metallica_-_Nothing_Else_Matters_(ColdMP3.com).wav");
         PlayerController.play();
         PlayerController.pause();
+        PlayerController.play();
+        PlayerController.next();
+        PlayerController.prev();
+        PlayerController.deleteSong("C:\\Users\\rusbe\\dev\\music-player-api\\src\\main\\java\\ru\\chabndheanee\\musicplayerapi\\src\\Metallica_-_Nothing_Else_Matters_(ColdMP3.com).wav");
     }
 }
